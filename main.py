02bb68fb7d016e9ca335a61444463e3f4c12b89e
Andrei Luiz de jesus <159576658+Andrei0769@users.noreply.github.com>
2024-10-14 13:34:38 +0000

    update; atv

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ session = Session()
 # Criando Tabela
 Base = declarative_base()
 class Cliente(Base):
-    __tablename__ = " Clientes"
+    __tablename__ = "clientes"
 
     # Definindo campos de tabela.
     id = Column("id", Integer, primary_key=True, autoincrement=True)
@@ -35,7 +35,7 @@ class Cliente(Base):
     email = Column("email", String)
     senha = Column("senha", String)
 
-    # Definindo atributos da classse 
+    # Definindo atributos da classe 
     def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome
         self.email = email
@@ -43,4 +43,69 @@ class Cliente(Base):
 
 # Criando tabela no banco de Dados.
 Base.metadata.create_all(bind=MEU_BANCO)
-    
+
+# CRUD.
+# Create -insert - Salvar.
+os.system("cls || clear") 
+
+print("Solicitando dados para o usuário.")
+inserir_nome = input("Digite o seu Nome: ")
+inserir_email = input("Digite o seu Email: ")
+inserir_senha = input("Digite o seu Senha: ")
+
+cliente = Cliente(nome=inserir_nome, email=inserir_email, senha=inserir_senha)
+session.add(cliente)
+session.commit()
+
+# Read - select - Consulta
+print("\nExibindo Dados de todos os Clientes.")
+lista_clientes = session.query(Cliente).all()
+
+for cliente in lista_clientes:
+    print(f"{cliente.id} - {cliente.nome} - {cliente.email} - {cliente.senha}")
+
+# U - Update - Atualizar 
+print("\nAtualizando dados do Usuário.")
+email_cliente = input("Digite o Email que será atualizado: ")
+
+cliente = session.query(Cliente).filter_by(email=email_cliente).first()
+
+if cliente:
+    cliente.nome = input("Digite o novo Nome: ")
+    cliente.email = input("Digite o novo e-mail: ")
+    cliente.senha = input("Digite a nova Senha: ")
+
+    session.commit()
+    print("Dados atualizados com sucesso.")
+else: 
+    print("Cliente não encontrado.")
+
+# Read - select - Consulta
+print("\nExibindo Dados de todos os Clientes.")
+lista_clientes = session.query(Cliente).all()
+
+for cliente in lista_clientes:
+    print(f"{cliente.id} - {cliente.nome} - {cliente.email} - {cliente.senha}")
+
+# D - Delete - DELETE - Excluir 
+print("\nExcluindo dados do Usuário.")
+email_cliente = input("Digite o Email do Cliente que será Excluido: ")
+
+cliente = session.query(Cliente).filter_by(email=email_cliente).first()
+
+if cliente:
+    session.delete(cliente)
+    session.commit()
+    print(f"Cliente {cliente.nome} excluído com sucesso!")
+else: 
+    print("Cliente não encontrado.")
+
+# Read - select - Consulta
+print("\nExibindo Dados de todos os Clientes.")
+lista_clientes = session.query(Cliente).all()
+
+for cliente in lista_clientes:
+    print(f"{cliente.id} - {cliente.nome} - {cliente.email} - {cliente.senha}")
+
+# Read - select - Consulta
+
